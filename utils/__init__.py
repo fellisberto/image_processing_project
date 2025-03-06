1706860b31d45d8ad60a181f3984ea6693fa1a49
fellisberto <enzo@fltcloudsync.net>
2025-03-06 11:12:51 -0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .image_utils import (
-    carregar_imagem_caminho,
-    exibir_imagem_cv2,
-    exibir_imagem_pil,
-    exibir_imagem_skimage,
-    converter_para_cinza,
-    redimensionar_imagem,
-    rotacionar_imagem,
-)
